2e93e5b7ae2516ddfb1790d1895799629f41525c
Antoinernf <antoinernf@gmail.com>
2021-08-06 09:52:09 +0200

    Modification de la sécurisation du mot de passe

--- backend/models/password.js
+++ backend/models/password.js
@@ -5,11 +5,11 @@ const passwordSchema = new passwordValidator();
 
 // Contraintes du mot de passe
 passwordSchema
-.is().min(8)                                    // Longueur minimun : 8
+.is().min(8)                                    // 8 caractères minimum
 .has().uppercase()                              // Doit avoir au moins une majuscule
 .has().lowercase()                              // Doit avoir au moins une minuscule
 .has().digits()                                 // Doit avoir au moins un chiffre
-.has().not().spaces()                           // Ne doit pas avoir d'espaces (pas sur que ce soit une bonne régle)
+.has().not().spaces()                           // Ne doit pas avoir d'espaces
 .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist de valeurs à proscrire
 
 module.exports = passwordSchema;
